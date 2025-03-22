b3eee5ddf7ae68c8015669130924b3ea52d9eeaf
Nicolas Takashi <nicolas.tcs@hotmail.com>
2025-03-22 21:12:47 +0000

    [CHORE] adding layout component
    Signed-off-by: Nicolas Takashi <nicolas.tcs@hotmail.com>

--- ui-v2/app/layout.tsx
+++ ui-v2/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import { ThemeProvider } from "@/components/theme-provider"
 import "./globals.css";
 
 const geistSans = Geist({
@@ -23,12 +24,20 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="dark"
+          enableSystem={true}
+          disableTransitionOnChange
+        >
+          {children}
+        </ThemeProvider>
       </body>
     </html>
   );
 }
+
