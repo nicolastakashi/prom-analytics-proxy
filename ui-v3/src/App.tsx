bf60c18c60cdbef7627eafaa496a4c99dc85f0aa
Nicolas Takashi <nicolas.tcs@hotmail.com>
2025-03-23 21:22:43 +0000

    [CHORE] project startup
    Signed-off-by: Nicolas Takashi <nicolas.tcs@hotmail.com>

--- ui-v3/src/App.tsx
+++ ui-v3/src/App.tsx
@@ -1,12 +1,11 @@
 import { Route, Switch, Link, Redirect } from "wouter";
-import { Overview } from "./pages/overview";
-import { Performance } from "./pages/performance";
-import { MetricsExplorer } from "./pages/metrics_explorer";
+import { Overview } from "@/pages/overview";
+import { Performance } from "@/pages/performance";
+import { MetricsExplorer } from "@/pages/metrics_explorer";
 
 function App() {
   return (
     <div>
-      {/* Navigation */}
       <nav>
         <Link href="/">Overview</Link>
         <Link href="/performance">Performance</Link>
@@ -18,7 +17,7 @@ function App() {
         <Route path="/" component={Overview} />
         <Route path="/performance" component={Performance} />
         <Route path="/metrics" component={MetricsExplorer} />
-        
+
         {/* Optional: Redirect any unknown routes to Overview */}
         <Route>
           <Redirect to="/" />
