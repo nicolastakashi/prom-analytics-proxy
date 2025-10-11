e24b863a0a07b9e780e2682eec67b8951bc9e7a1
Nicolas Takashi <nicolas.tcs@hotmail.com>
2025-10-11 14:31:41 +0100

    [FEATURE] adding metrics ingester endpoint
    Signed-off-by: Nicolas Takashi <nicolas.tcs@hotmail.com>

--- main.go
+++ main.go
@@ -15,6 +15,7 @@ import (
 	"github.com/oklog/run"
 
 	"github.com/nicolastakashi/prom-analytics-proxy/cmd/api"
+	"github.com/nicolastakashi/prom-analytics-proxy/cmd/ingester"
 	"github.com/nicolastakashi/prom-analytics-proxy/internal/config"
 	"github.com/nicolastakashi/prom-analytics-proxy/internal/log"
 	"github.com/nicolastakashi/prom-analytics-proxy/internal/tracing"
@@ -33,42 +34,67 @@ func main() {
 	)
 
 	apiFlagSet := flag.NewFlagSet("api", flag.ExitOnError)
-	apiFlagSet.Usage = func() {
+	ingesterFlagSet := flag.NewFlagSet("ingester", flag.ExitOnError)
+
+	printRootUsage := func() {
 		fmt.Fprintf(os.Stderr, "Usage:\n  %s [command] [flags]\n\n", os.Args[0])
 		fmt.Fprintln(os.Stderr, "Commands:")
-		fmt.Fprintln(os.Stderr, "  api     Run the proxy server (default)")
+		fmt.Fprintln(os.Stderr, "  api       Run the proxy server (default)")
+		fmt.Fprintln(os.Stderr, "  ingester  Run metrics ingester (experimental)")
 		fmt.Fprintln(os.Stderr, "")
-		fmt.Fprintln(os.Stderr, "Flags:")
+		fmt.Fprintln(os.Stderr, "Flags (api):")
 		apiFlagSet.PrintDefaults()
+		fmt.Fprintln(os.Stderr, "")
+		fmt.Fprintln(os.Stderr, "Flags (metrics ingester):")
+		ingesterFlagSet.PrintDefaults()
 	}
+
+	apiFlagSet.Usage = printRootUsage
 	log.RegisterFlags(apiFlagSet)
 
 	api.RegisterFlags(apiFlagSet, &configFile)
 
+	ingesterFlagSet.Usage = func() {
+		fmt.Fprintf(os.Stderr, "Usage:\n  %s ingester [flags]\n\n", os.Args[0])
+		fmt.Fprintln(os.Stderr, "Flags (metrics ingester):")
+		ingesterFlagSet.PrintDefaults()
+	}
+	log.RegisterFlags(ingesterFlagSet)
+	ingester.RegisterFlags(ingesterFlagSet, &configFile)
+
 	args := os.Args[1:]
+	cmd := "api"
 	if len(args) > 0 {
 		first := args[0]
 		if first == "-h" || first == "--help" || first == "help" {
-			apiFlagSet.Usage()
+			printRootUsage()
 			os.Exit(0)
 		}
-		if first == "api" {
+		if first == "api" || first == "ingester" {
+			cmd = first
 			args = args[1:]
 			if len(args) > 0 && (args[0] == "-h" || args[0] == "--help" || args[0] == "help") {
-				apiFlagSet.Usage()
+				if cmd == "api" {
+					apiFlagSet.Usage()
+				} else {
+					ingesterFlagSet.Usage()
+				}
 				os.Exit(0)
 			}
 		} else if !strings.HasPrefix(first, "-") {
 			fmt.Fprintf(os.Stderr, "unknown command %q\n\n", first)
-			fmt.Fprintln(os.Stderr, "Available command: api (default). You can run it explicitly as:")
-			fmt.Fprintln(os.Stderr, "  prom-analytics-proxy api [flags]")
-			fmt.Fprintln(os.Stderr, "")
-			apiFlagSet.Usage()
+			printRootUsage()
 			os.Exit(2)
 		}
 	}
 
-	err := apiFlagSet.Parse(args)
+	var err error
+	switch cmd {
+	case "api":
+		err = apiFlagSet.Parse(args)
+	case "ingester":
+		err = ingesterFlagSet.Parse(args)
+	}
 	if err != nil {
 		fmt.Printf("error parsing flags: %v", err)
 		os.Exit(1)
@@ -80,40 +106,50 @@ func main() {
 		os.Exit(1)
 	}
 	slog.SetDefault(logger)
-
 	if configFile != "" {
-		err := config.LoadConfig(configFile)
-		if err != nil {
+		if err := config.LoadConfig(configFile); err != nil {
 			slog.Error("unable to load config file", "err", err)
 			os.Exit(1)
 		}
 	}
-
+	var shutdown func()
 	if config.DefaultConfig.IsTracingEnabled() {
 		tp, err := tracing.WithTracing(context.Background(), logger, configFile)
 		if err != nil {
 			slog.Error("unable to create tracer provider", "err", err)
 			os.Exit(1)
 		}
-
-		defer func() {
+		shutdown = func() {
 			if err := tp.Shutdown(context.Background()); err != nil {
 				slog.Info("Error shutting down tracer provider", "err", err)
 			}
-		}()
+		}
+	} else {
+		shutdown = func() {}
 	}
+	defer shutdown()
 
-	uiFS, err := loadEmbedFS("ui/dist")
-	if err != nil {
-		slog.Error("unable to load embed FS", "err", err)
-		os.Exit(1)
-	}
-
-	if err := api.Run(uiFS); err != nil {
-		if !errors.As(err, &run.SignalError{}) {
-			slog.Error("server stopped", "err", err)
+	switch cmd {
+	case "api":
+		uiFS, err := loadEmbedFS("ui/dist")
+		if err != nil {
+			slog.Error("unable to load embed FS", "err", err)
 			os.Exit(1)
 		}
-		slog.Info("caught signal; exiting gracefully...")
+		if err := api.Run(uiFS); err != nil {
+			if !errors.As(err, &run.SignalError{}) {
+				slog.Error("server stopped", "err", err)
+				os.Exit(1)
+			}
+			slog.Info("caught signal; exiting gracefully...")
+		}
+	case "ingester":
+		if err := ingester.Run(); err != nil {
+			if !errors.As(err, &run.SignalError{}) {
+				slog.Error("ingester stopped", "err", err)
+				os.Exit(1)
+			}
+			slog.Info("ingester: caught signal; exiting gracefully...")
+		}
 	}
 }
