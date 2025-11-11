d8c36737e7a4f3decd98136af75c0fdfe4557c57
Martin Chodur <m.chodur@seznam.cz>
2025-11-11 01:33:51 +0100

    feat: add ingester metrics
    Signed-off-by: Martin Chodur <m.chodur@seznam.cz>

--- internal/ingester/queryIngester.go
+++ internal/ingester/queryIngester.go
@@ -9,6 +9,8 @@ import (
 
 	"github.com/nicolastakashi/prom-analytics-proxy/internal/db"
 	"github.com/nicolastakashi/prom-analytics-proxy/internal/promfp"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promauto"
 	"github.com/prometheus/prometheus/promql/parser"
 	"go.opentelemetry.io/otel"
 )
@@ -24,6 +26,10 @@ type QueryIngester struct {
 	ingestTimeout       time.Duration
 	batchSize           int
 	batchFlushInterval  time.Duration
+
+	queriesIngestedTotal *prometheus.CounterVec
+	droppedQueriesTotal  *prometheus.CounterVec
+	batchSizeHistogram   prometheus.Histogram
 }
 
 type QueryIngesterOption func(*QueryIngester)
@@ -58,7 +64,7 @@ func WithBatchFlushInterval(interval time.Duration) QueryIngesterOption {
 	}
 }
 
-func NewQueryIngester(dbProvider db.Provider, opts ...QueryIngesterOption) *QueryIngester {
+func NewQueryIngester(reg prometheus.Registerer, dbProvider db.Provider, opts ...QueryIngesterOption) *QueryIngester {
 	qi := &QueryIngester{
 		dbProvider: dbProvider,
 	}
@@ -66,6 +72,31 @@ func NewQueryIngester(dbProvider db.Provider, opts ...QueryIngesterOption) *Quer
 	for _, opt := range opts {
 		opt(qi)
 	}
+
+	if reg == nil {
+		reg = prometheus.DefaultRegisterer
+	}
+	qi.droppedQueriesTotal = promauto.With(reg).NewCounterVec(
+		prometheus.CounterOpts{
+			Name: "query_ingester_dropped_queries_total",
+			Help: "Total number of dropped queries due to full buffer or closed ingester",
+		},
+		[]string{"reason"},
+	)
+	qi.batchSizeHistogram = promauto.With(reg).NewHistogram(
+		prometheus.HistogramOpts{
+			Name:    "query_ingester_batch_size",
+			Help:    "Histogram of batch sizes ingested",
+			Buckets: prometheus.ExponentialBucketsRange(1, float64(qi.batchSize), 10),
+		},
+	)
+	qi.queriesIngestedTotal = promauto.With(reg).NewCounterVec(
+		prometheus.CounterOpts{
+			Name: "query_ingester_queries_ingested_total",
+			Help: "Total number of queries ingested",
+		},
+		[]string{"type"},
+	)
 
 	return qi
 }
@@ -75,14 +106,14 @@ func (i *QueryIngester) Ingest(query db.Query) {
 	defer i.mu.RUnlock()
 
 	if i.closed {
-		//TODO(nicolastakashi): expose this to a metric
+		i.droppedQueriesTotal.WithLabelValues("closed").Inc()
 		slog.Error(fmt.Sprintf("closed: dropping query: %v", query))
 		return
 	}
 	select {
 	case i.queriesC <- query:
 	default:
-		//TODO(nicolastakashi): expose this to a metric
+		i.droppedQueriesTotal.WithLabelValues("blocked").Inc()
 		slog.Error(fmt.Sprintf("blocked: dropping query: %v", query))
 	}
 }
@@ -103,6 +134,7 @@ func (i *QueryIngester) Run(ctx context.Context) {
 			i.drainWithGracePeriod(batch)
 			return
 		case query := <-i.queriesC:
+			i.queriesIngestedTotal.WithLabelValues(string(query.Type)).Inc()
 			query.Fingerprint = fingerprintFromQuery(query.QueryParam)
 			query.LabelMatchers = labelMatchersFromQuery(query.QueryParam)
 
@@ -141,9 +173,9 @@ func (i *QueryIngester) ingest(ctx context.Context, queries []db.Query) {
 	ingestCtx, ingestCancel := context.WithTimeout(ctx, i.ingestTimeout)
 	defer ingestCancel()
 
+	i.batchSizeHistogram.Observe(float64(len(queries)))
 	traceContext, span := otel.Tracer("query-ingester").Start(ingestCtx, "ingest")
 	defer span.End()
-
 	err := i.dbProvider.Insert(traceContext, queries)
 	if err != nil {
 		slog.Error("unable to insert query", "err", err)
