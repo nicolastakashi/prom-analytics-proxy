42051ac603cfd1ed2f94c06d4e50943ae3de218d
Guillaume LADORME <Gladorme@users.noreply.github.com>
2026-03-02 17:25:02 +0100

    Add React Compiler
    Signed-off-by: Guillaume LADORME <Gladorme@users.noreply.github.com>

--- ui/vite.config.ts
+++ ui/vite.config.ts
@@ -5,7 +5,14 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react({
+      babel: {
+        plugins: ["babel-plugin-react-compiler"],
+      },
+    }),
+    tailwindcss(),
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
